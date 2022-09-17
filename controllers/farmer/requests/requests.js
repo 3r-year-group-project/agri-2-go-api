a6d15d5757dd2b3bf3f9b175d2017bd38baac270
ChathuraAlwis <chathura7alwis@gmail.com>
2022-09-17 13:05:17 +0530

    show newly added requests

--- controllers/farmer/requests/requests.js
+++ controllers/farmer/requests/requests.js
@@ -15,7 +15,7 @@ exports.sentRequsts = (req, res, next) => {
         console.log(data);
         let id = data[0].id;
 
-        sql = "SELECT request_id, price, quantity, economic_center, id as economic_center_id, vegetable, declines FROM (SELECT request_id, price, quantity, economic_center, vegetable, COUNT(request_id) as declines FROM selling_request INNER JOIN decline_request ON selling_request.id=decline_request.request_id WHERE farmer_id=? and status=? GROUP BY request_id) AS A INNER JOIN economic_center ON A.economic_center=economic_center.name;";
+        sql = "SELECT S.id as request_id, S.price, S.quantity, S.vegetable, E.id as economic_center_id, E.name as economic_center FROM selling_request as S INNER JOIN economic_center as E ON s.economic_center=e.name WHERE S.farmer_id=? AND S.status=?;";
         let q = conn.query(sql, [id, REQUEST_STATE.ACTIVE], (err, data1) => {
             if(err) return next(new AppError(err,500));
             res.status(200).json({
@@ -35,6 +35,25 @@ exports.declinesLimit = (req, res, next) => {
         let id = data[0].id;
 
         sql = "SELECT eco_center_id, COUNT(id) as shop_count FROM shop GROUP BY eco_center_id;";
+        let q = conn.query(sql, (err, data1) => {
+            if(err) return next(new AppError(err,500));
+            res.status(200).json({
+                status: 'successfully get the decline limit',
+                data: data1
+            });
+        });  
+        console.log(q.sql);     
+    }
+    );  
+};
+
+exports.declinesCount = (req, res, next) => {
+    let sql = "SELECT id FROM user WHERE email=?"
+    conn.query(sql, [req.body.email], (err, data) => {
+        if(err) return next(new AppError(err,500));
+        let id = data[0].id;
+
+        sql = "SELECT request_id, COUNT(id) AS declines_count FROM decline_request GROUP BY request_id;";
         let q = conn.query(sql, (err, data1) => {
             if(err) return next(new AppError(err,500));
             res.status(200).json({
