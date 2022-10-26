20691a9ca9f5f51ac6630220191d5a9f1c6d8cf9
SethniBhagya <sethnibhagya67@gmail.com>
2022-10-26 15:52:13 +0530

    update wrc

--- routes/wrc/index.js
+++ routes/wrc/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../../controllers/wrc/registration/register');
 const wastageController = require('../../controllers/wastage/index');
+
+const dashboardController = require('../../controllers/users/dashboard/index')
+
+router.route('/dashboard/best_selling_items')
+    .get(dashboardController.bestSales)
+
+router.route('/dashboard/user_counts')
+    .get(dashboardController.userCounts)
+
 router.route('/registration/paymentPlan')
     .post(controllers.paymentplanInsert);
 
